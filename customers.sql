556f32907157ccb305d7c66d6fc365e498082664
Michael Ferguson <ubuntu@ip-172-31-18-227.us-east-2.compute.internal>
2021-09-02 06:30:12 +0000

    Add customers query

---
+++ customers.sql
@@ -0,0 +1,10 @@
+select 
+o.customer_id,
+c.name,
+c.email, 
+min(o.created_at) as first_order_at,
+count(*) as number_of_orders,
+from analytics-engineers-club.coffee_shop.orders o 
+join analytics-engineers-club.coffee_shop.customers c on o.customer_id = c.id
+group by o.customer_id, c.name, c.email
+order by first_order_at
